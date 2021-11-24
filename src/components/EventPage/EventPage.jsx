c86dbb19f599d3262adadcd1ea5d8f6d1592c471
mariagluck <mariamart.berlin@gmail.com>
2021-11-24 17:18:48 +0100

    single event page

--- src/components/EventPage/EventPage.jsx
+++ src/components/EventPage/EventPage.jsx
@@ -1,7 +1,13 @@
-import Header from "../NavBar/NavBar"
-import Footer from "../Footer/Footer"
+import Header from "../NavBar/NavBar";
+import Footer from "../Footer/Footer";
 import SearchNav from "../SearchNav/SearchNav";
-import "./EventPage.scss"
+
+import "./EventPage.scss";
+import {FaCalendarAlt} from "react-icons/fa";
+import {FaMapMarkerAlt} from "react-icons/fa";
+import {FaTicketAlt} from "react-icons/fa";
+import {FaInfo} from "react-icons/fa";
+import eventImage from "../../images/mockup_event_0.jpeg";
 
 
 export default function EventPage(){
@@ -13,7 +19,46 @@ export default function EventPage(){
                 <SearchNav />
             </div>
             <main>
-                
+             <div className="event-container">
+                <div className="event-image">
+                    <img src={eventImage} alt="" />
+                </div>
+                <div className="event-info">
+                    <div className="dates">
+                        <div className="item-info">
+                             <h5>Dates and Time</h5>  
+                             <p>Sat 20 Nov    11pm</p>
+                        </div>
+                     
+                       <FaCalendarAlt />
+                    </div>
+                    <div className="location">
+                    <div className="item-info">
+                       <h5> Location</h5> 
+                       <p>Prinzenstraße 85F</p>
+                       <p>10969 Berlin</p>  
+                       </div>
+                       <FaMapMarkerAlt /> 
+                    </div>
+                    <div className="price">
+                     <div className="item-info">
+                       <h5>Price and Bookings</h5>
+                       <p>50 €</p>
+                     </div>
+                      <FaTicketAlt />
+                    </div>
+                    <div className="contact-i">
+                     <div className="item-info">
+                       <h5>Contact details</h5>
+                       <p>info@horsemeatdisco.com</p>
+                     </div>
+                     <FaInfo />
+                    </div>
+                </div>
+                <div className="event-about">
+                </div>
+                <div className="event-Map"></div>
+             </div>
             </main>
            
             <Footer />
