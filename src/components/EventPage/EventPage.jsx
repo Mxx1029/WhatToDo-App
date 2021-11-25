4709285c82616c268d8d4a8610ff2cdaae48f2a7
mariagluck <mariamart.berlin@gmail.com>
2021-11-25 10:44:21 +0100

    single event page done

--- src/components/EventPage/EventPage.jsx
+++ src/components/EventPage/EventPage.jsx
@@ -22,12 +22,13 @@ export default function EventPage(){
              <div className="event-container">
                 <div className="event-image">
                     <img src={eventImage} alt="" />
+                    <p className="event-date">13 NOV</p>
                 </div>
                 <div className="event-info">
                     <div className="dates">
                         <div className="item-info">
                              <h5>Dates and Time</h5>  
-                             <p>Sat 20 Nov    11pm</p>
+                             <p>Sat 20 Nov  |  11pm</p>
                         </div>
                      
                        <FaCalendarAlt />
@@ -35,8 +36,10 @@ export default function EventPage(){
                     <div className="location">
                     <div className="item-info">
                        <h5> Location</h5> 
-                       <p>Prinzenstraße 85F</p>
-                       <p>10969 Berlin</p>  
+                       <p>Prinzenstraße 85F,  10969 Berlin</p>
+                       <br />
+                       <a href="https://googlemaps.com">Open in map</a> {/* this is a web link */}
+                    
                        </div>
                        <FaMapMarkerAlt /> 
                     </div>
@@ -51,13 +54,28 @@ export default function EventPage(){
                      <div className="item-info">
                        <h5>Contact details</h5>
                        <p>info@horsemeatdisco.com</p>
+                       <br />
+                       <a href="https://eventpage.com">visit website</a>{/* this is a web link */}
                      </div>
                      <FaInfo />
                     </div>
                 </div>
                 <div className="event-about">
+                    <h2>Event Title</h2>
+                    <div className="event-description"> 
+                        <p>The nation’s foremost celebration of everything drag, kitsch and pop-culture comes to Melbourne.
+                        Catch up with local and international drag artiste royalty including Drag Expo Ambassador Karen From Finance. Visitors can meet the stars of RuPaul’s Drag Race, plus local stars. Some of the stars include Alaska Thunder*uck, Art Simone, Bebe Zahara Benet, Berry Juicy, Bible Girl, Chelsea Bun, Cherylyn Barnes, Desmond Is Amazing, Eve Elle, Hannah Conda, Jasmine Masters, Kane Enable, Katya, Luci Furr, Manila Luzon, Pangina Heels, Phi Phi O'Hara, Sharon Needles, Stacy Queen, Tatianna, Trinity The Tuck, Trixie Mattel and Wundes.
+                        
+                        Enjoy a plethora of panels, tutorials, chill-out spaces, fabulous shopping stores, runways with Q&A sessions, meet and greets, official merchandise, a drag market and more.
+                        
+                        Learn how drag has evolved over the years, how Australia has given drag our own special flavour, and how drag artistes have a message everyone can learn from.
+                        </p> 
+                    </div>
                 </div>
-                <div className="event-Map"></div>
+                <div className="event-Map">
+                <p>This is the map container and here goes a map view</p> 
+                </div>
+                
              </div>
             </main>
            
