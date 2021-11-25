6eaed312ef02eb098b2618245985f0ce3fc877fa
mariagluck <mariamart.berlin@gmail.com>
2021-11-25 16:20:56 +0100

    fetch data cardlist done and on development

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,12 +1,39 @@
-import Header from "../NavBar/NavBar"
-import Footer from "../Footer/Footer"
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import Header from "../NavBar/NavBar";
+import Footer from "../Footer/Footer";
 import SearchNav from "../SearchNav/SearchNav";
 import Card from "../Card/Card";
 import "./LandingPage.scss"
 
 
 export default function LandingPage(){
+    const [events, setEvents] = useState([]);
+    const [loading, setLoading] = useState(true);
+    
+    useEffect(() => {
+        fetchEvents();
+        setLoading(false);
+      }, []);
 
+    const fetchEvents = () => {
+        axios
+          .get('http://localhost:9012/events/today')
+          .then((res) => {
+            console.log(res);
+            setEvents(res.data);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      };
+
+    if (loading) {
+        return "Loading...";
+    }
+    
+    console.log(events);
+    
     return(
         <div className="search-nav">
             <div className="top">
@@ -17,18 +44,20 @@ export default function LandingPage(){
                 <h2>WHAT TO DO ...TODAY?</h2>
             </div>
             <main>
-                <Card /> 
-                <Card /> 
-                <Card /> 
-                <Card /> 
-                <Card /> 
-                <Card /> 
-                <Card /> 
-                <Card />
-                <Card /> 
-                <Card /> 
-                <Card /> 
-                <Card />
+              {events?.map(event => 
+                 <Card key={event._id} 
+                       event={event} />)}   
+           
+                  {/* <Card 
+                  //    name={`${eventList.title}`}
+                  //    eventImage={eventList.picture.thumbnail}
+                  //    date={eventList.date}
+                  //    about={eventList.email}
+                  //    cathegory={eventList.cathegory}
+                  //    key={eventList.id}  
+                     /> 
+              */}
+                
             </main>
            
             <Footer />
